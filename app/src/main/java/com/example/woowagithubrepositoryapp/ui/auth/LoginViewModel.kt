0bab4ade9ea6ef2c37b67082733274a2a6f62207
Skipancho <ctg1770a@gmail.com>
2022-07-11 20:06:46 +0900

    #2 로그인 - code 값 받아오기

---
+++ app/src/main/java/com/example/woowagithubrepositoryapp/ui/auth/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.woowagithubrepositoryapp.ui.auth
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    val code = MutableLiveData("")
+}
