db0473947b18f293c5469cad0410eff17e101feb
Skipancho <ctg1770a@gmail.com>
2022-07-11 23:56:21 +0900

    #2 로그인 - repository 분리

--- app/src/main/java/com/example/woowagithubrepositoryapp/repository/GithubRepository.kt
+++ app/src/main/java/com/example/woowagithubrepositoryapp/repository/GithubRepository.kt
@@ -1,9 +1,7 @@
 package com.example.woowagithubrepositoryapp.repository
+
 import com.example.woowagithubrepositoryapp.network.GithubService
-import com.example.woowagithubrepositoryapp.network.TokenApi
 
 class GithubRepository {
-    suspend fun getAccessToken(
-        code : String
-    ) = TokenApi.instance.getAccessToken(code = code)
+    suspend fun getUserData() = GithubService.instance.getUserData()
 }
--- app/src/main/java/com/example/woowagithubrepositoryapp/ui/auth/LoginViewModel.kt
+++ app/src/main/java/com/example/woowagithubrepositoryapp/ui/auth/LoginViewModel.kt
@@ -1,12 +1,10 @@
 package com.example.woowagithubrepositoryapp.ui.auth
 
-import android.content.Intent
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.woowagithubrepositoryapp.App
-import com.example.woowagithubrepositoryapp.repository.GithubRepository
+import com.example.woowagithubrepositoryapp.repository.TokenRepository
 import com.example.woowagithubrepositoryapp.ui.MainActivity
 import com.example.woowagithubrepositoryapp.utils.Prefs
 import com.example.woowagithubrepositoryapp.utils.clearTasksAndStartActivity
@@ -14,16 +12,15 @@ import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
     val code = MutableLiveData("")
-    val repository = GithubRepository()
+    private val repository = TokenRepository()
 
     fun getToken() = viewModelScope.launch {
         try {
             val response = repository.getAccessToken(code.value.toString())
             val body = response.body()
-            if (body != null){
+            if (response.isSuccessful && body != null){
                 Prefs.accessToken = body.accessToken
                 Prefs.code = code.value.toString()
-
                 clearTasksAndStartActivity<MainActivity>()
             }
         }catch (e : Exception){
