aa55d23886cf658c1a1b137654f4494dea48a8b6
Skipancho <ctg1770a@gmail.com>
2022-07-12 09:52:07 +0900

    #2 로그인 - Prefs 에서 code 제거 (사유 : 불필요)

--- app/src/main/java/com/example/woowagithubrepositoryapp/network/RefreshTokenInterceptor.kt
+++ app/src/main/java/com/example/woowagithubrepositoryapp/network/RefreshTokenInterceptor.kt
@@ -11,14 +11,13 @@ class RefreshTokenInterceptor : Interceptor{
     override fun intercept(chain: Interceptor.Chain): Response {
         val original = chain.request()
         val request = original.newBuilder().apply {
-            header("Authorization", Prefs.code)
+            header("Authorization", "token ${Prefs.accessToken}")
             method(original.method,original.body)
         }.build()
 
         val response = chain.proceed(request)
 
         if (response.code == 401){
-            Prefs.code = ""
             Prefs.accessToken = ""
             val intent = Intent(App.instance,LoginActivity::class.java).apply {
                 addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
--- app/src/main/java/com/example/woowagithubrepositoryapp/utils/Prefs.kt
+++ app/src/main/java/com/example/woowagithubrepositoryapp/utils/Prefs.kt
@@ -5,7 +5,6 @@ import com.example.woowagithubrepositoryapp.App
 
 object Prefs {
     private const val ACCESS_TOKEN = "access_token"
-    private const val CODE = "code"
 
     private val prefs by lazy {
         PreferenceManager.getDefaultSharedPreferences(App.instance)
@@ -14,8 +13,4 @@ object Prefs {
     var accessToken
         get() = prefs.getString(ACCESS_TOKEN, null).toString()
         set(value) = prefs.edit().putString(ACCESS_TOKEN, value).apply()
-
-    var code
-        get() = prefs.getString(CODE, null).toString()
-        set(value) = prefs.edit().putString(CODE, value).apply()
 }
