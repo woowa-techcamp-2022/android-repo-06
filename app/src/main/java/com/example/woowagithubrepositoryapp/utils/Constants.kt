3763438e88eaa5d57203dc09f7991076cbf0b0ce
eshc123 <eshc.joon@gmail.com>
2022-07-12 09:20:22 +0900

    #2 로그인 - local.properties 값 불러오기

--- app/src/main/java/com/example/woowagithubrepositoryapp/utils/Constants.kt
+++ app/src/main/java/com/example/woowagithubrepositoryapp/utils/Constants.kt
@@ -1,5 +1,8 @@
 package com.example.woowagithubrepositoryapp.utils
 
-object Constants {
+import com.example.woowagithubrepositoryapp.BuildConfig
 
+object Constants {
+    const val GITHUB_CLIENT_ID = BuildConfig.githubClientId
+    const val GITHUB_CLIENT_SECRET = BuildConfig.githubClientSecret
 }
