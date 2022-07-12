dc1f5a894af813918df477e5625ecfe5295b7321
eshc123 <eshc.joon@gmail.com>
2022-07-12 11:02:03 +0900

    #3 메인 - 메인 액티비티 초기 구성, 탭 레이아웃 추가

--- app/src/main/java/com/example/woowagithubrepositoryapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/woowagithubrepositoryapp/ui/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.woowagithubrepositoryapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.woowagithubrepositoryapp.R
+import com.example.woowagithubrepositoryapp.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
+    }
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        initTabLayout(binding.tablayoutMain)
+    }
+
+    private fun initTabLayout(tabLayout: TabLayout){
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.issue))
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.notifications))
     }
 }
