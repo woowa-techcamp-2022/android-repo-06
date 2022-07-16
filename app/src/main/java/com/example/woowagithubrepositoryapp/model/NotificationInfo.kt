4d234a6d1b745fef49e0c2781432d1dd906ed28a
eshc123 <eshc.joon@gmail.com>
2022-07-16 13:19:07 +0900

    #38 알림 - Notification Adapter 수정, NotificationInfo 관련 이름 변경

--- app/src/main/java/com/example/woowagithubrepositoryapp/model/NotificationInfo.kt
+++ app/src/main/java/com/example/woowagithubrepositoryapp/model/NotificationInfo.kt
@@ -2,7 +2,7 @@ package com.example.woowagithubrepositoryapp.model
 
 import com.google.gson.annotations.SerializedName
 
-data class NotificationSubjectURLResponse(
+data class NotificationInfo(
     @SerializedName("number") val number : Int,
     @SerializedName("comments") val comments : Int,
 )
