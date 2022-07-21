bf884d547e8b722b8c8f53e45dde33539d9ecbff
Skipancho <ctg1770a@gmail.com>
2022-07-21 11:22:00 +0900

    #28 refactor - 불필요 코드라인 제거

--- app/src/main/java/com/example/woowagithubrepositoryapp/utils/TimeUtil.kt
+++ app/src/main/java/com/example/woowagithubrepositoryapp/utils/TimeUtil.kt
@@ -1,21 +1,18 @@
 package com.example.woowagithubrepositoryapp.utils
 
-import android.annotation.SuppressLint
-import android.util.Log
-import java.lang.Math.abs
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.abs
 
 object TimeUtil {
     private const val SECOND = 1000
     private const val MINUTE = SECOND * 60
     private const val HOUR = MINUTE * 60
     private const val DAY = HOUR * 24
-    private const val WEEK = DAY * 7
 
-    fun getTimeData(updated: String) : String {
+    fun getTimeData(updated: String): String {
         try {
-            val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss",Locale.KOREAN)
+            val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.KOREAN)
             val currentDate = Calendar.getInstance().apply {
                 timeInMillis = Date().time
             }
@@ -24,15 +21,7 @@ object TimeUtil {
                 timeInMillis = parser.parse(updated)?.time ?: 0L
             }
 
-            val difference = abs(currentDate.timeInMillis - targetDate.timeInMillis)
-
-            return when(difference){
-/*                in 0 until MINUTE -> {
-                    "${(difference / SECOND).toInt()}초 전"
-                }
-                in MINUTE until HOUR -> {
-                    "${(difference / MINUTE).toInt()}분 전"
-                }*/
+            return when (val difference = abs(currentDate.timeInMillis - targetDate.timeInMillis)) {
                 in 0 until DAY -> {
                     "오늘"
                 }
@@ -40,7 +29,7 @@ object TimeUtil {
                     "${(difference / DAY).toInt()}일 전"
                 }
             }
-        }catch (e : Exception){
+        } catch (e: Exception) {
             return updated
         }
     }
