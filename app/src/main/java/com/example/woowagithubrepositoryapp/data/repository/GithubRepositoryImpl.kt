91f5f808966ed054711201cb554ef6f28dcd00ee
eshc123 <eshc.joon@gmail.com>
2022-07-22 11:13:09 +0900

    #65 github Datasource Impl 수정

--- app/src/main/java/com/example/woowagithubrepositoryapp/data/repository/GithubRepositoryImpl.kt
+++ app/src/main/java/com/example/woowagithubrepositoryapp/data/repository/GithubRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.example.woowagithubrepositoryapp.data.repository
 
 import android.util.Log
+import com.example.woowagithubrepositoryapp.data.datasource.GithubDataSource
+import com.example.woowagithubrepositoryapp.data.datasource.GithubRemoteDataSourceImpl
 import com.example.woowagithubrepositoryapp.data.network.GithubClient
 import com.example.woowagithubrepositoryapp.data.network.GithubService
 import com.example.woowagithubrepositoryapp.model.*
@@ -9,116 +11,70 @@ import java.io.IOException
 
 class GithubRepositoryImpl : GithubRepository {
     private val service = GithubClient().generate(GithubService::class.java)
+    private val dataSource = GithubRemoteDataSourceImpl()
 
-    override suspend fun getUserData(): Result<User> = withContext(Dispatchers.IO) {
-        try {
-            val response = service.getUserData()
-            val body = response.body()
-            if (response.isSuccessful && body != null) {
-                val starredReposCnt = getStarredRepos()
-                body.apply {
-                    starredCnt = starredReposCnt
-                }
-                Result.success(body)
-            } else Result.failure(Exception("Get User Data Error"))
-        } catch (e: Exception) {
+    override suspend fun getUserData(): Result<User> {
+        return try {
+            val getUserDataResult = dataSource.getUserData()
+            if(getUserDataResult.isSuccess){
+                val userData = getUserDataResult.getOrThrow()
+                val starredRepos = dataSource.getStarredRepos().getOrThrow()
+                Result.success(userData.apply {
+                    this.starredCnt = starredRepos
+                })
+            }
+            else {
+                getUserDataResult
+            }
+        }
+        catch (e : Exception){
             Result.failure(e)
         }
     }
 
-    override suspend fun getNotifications(page: Int): Result<MutableList<Notification>> =
-        withContext(Dispatchers.IO) {
-            supervisorScope {
-                try {
-                    val response = service.getNotifications(page = page)
-                    if (response.isSuccessful) {
-                        val notifications = response.body()?.toMutableList() ?: mutableListOf()
-                        val notificationInfos = notifications.map {
-                            async {
-                                getNotificationInfo(it.subject.url, it.id)
-                            }
-                        }.awaitAll()
-
-                        Result.success(notifications.zip(notificationInfos) { notification, notificationInfo ->
-                            notification.apply {
-                                this.comments = notificationInfo?.comments.toString()
-                                this.issueNum = "#${notificationInfo?.number}"
-                            }
-                        }.toMutableList())
-                    } else Result.failure(Exception("Get Notifications Error : response isn't successful"))
-                } catch (e: Exception) {
-                    Log.d("getNotiError", e.cause.toString())
-                    Result.failure(e)
-                }
-            }
+    override suspend fun getNotifications(page: Int)
+    : Result<MutableList<Notification>> = supervisorScope {
+        return@supervisorScope try {
+            val getNotificationResult = dataSource.getNotifications(page)
+            if (getNotificationResult.isSuccess) {
+                val notifications = getNotificationResult.getOrThrow()
+                val notificationInfo = notifications.map {
+                    async {
+                        dataSource.getNotificationInfo(it.subject.url).getOrThrow()
+                    }
+                }.awaitAll()
+                Result.success(notifications.zip(notificationInfo) { noti, notiInfo ->
+                    noti.apply {
+                        this.comments = notiInfo?.comments.toString()
+                        this.issueNum = "#${notiInfo?.number}"
+                    }
+                }.toMutableList())
+            } else
+                getNotificationResult
         }
+        catch(e: Exception) {
+            Result.failure(e)
+        }
+    }
 
     override suspend fun patchNotificationThread(
         threadId: String
-    ): Result<Boolean> = withContext(Dispatchers.IO) {
-        try {
-            val response = service.patchNotificationThread(threadId)
-            Result.success(response.isSuccessful)
-        } catch (e: Exception) {
-            Log.d("patchNotiThreadError", e.cause.toString())
-            Result.failure(e)
-        }
+    ): Result<Boolean> {
+        return dataSource.patchNotificationThread(threadId)
     }
 
     override suspend fun getUserIssues(
         state: String,
         page: Int
-    ): Result<List<Issue>> = withContext(Dispatchers.IO) {
-        try {
-            val response = service.getIssues(
-                state = state,
-                page = page
-            )
-            val body = response.body()
-            if (response.isSuccessful && body != null)
-                Result.success(body)
-            else
-                Result.failure(Exception("Get User Issues Error"))
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
+    ): Result<List<Issue>> {
+        return dataSource.getUserIssues(state, page)
     }
 
     override suspend fun searchRepos(
         searchText: String,
         page: Int
-    ): Result<RepoResponse> = withContext(Dispatchers.IO) {
-        try {
-            val response = service.searchRepositories(
-                searchText = searchText,
-                page = page
-            )
-            val body = response.body()
-            if (response.isSuccessful && body != null)
-                Result.success(body)
-            else Result.failure(Exception("Search Repos Error"))
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
-
-    private suspend fun getNotificationInfo(fullUrl: String, id: String): NotificationInfo? {
-        return try {
-            service.getNotificationInfo(fullUrl).body().apply {
-                this?.notificationId = id
-            }
-        } catch (e: Exception) {
-            Log.d("getNotiInfoError", e.cause.toString())
-            null
-        }
-    }
-
-    private suspend fun getStarredRepos(): Int {
-        val response = service.getStarredRepos()
-        val body = response.body()
-        return if (response.isSuccessful && body != null) {
-            body.size
-        } else 0
+    ): Result<RepoResponse> {
+        return dataSource.getRepos(searchText, page)
     }
 
     companion object {
